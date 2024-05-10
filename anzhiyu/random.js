6909fbd63e36b2980ea6e59446ac830810013198
LuoC33 <3399159833@qq.com>
2024-05-10 23:41:36 +0800

    Site updated: 2024-05-10 23:41:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/05/OCS网课助手实现网课自动刷课/","2024/05/05/同款博客搭建教程/","2024/05/09/虚拟机安装W7-W10优化精简版/","2024/05/10/审查元素强制绕过批改网复制限制教程/"];function toRandomPost(){
+var posts=["2024/05/05/OCS网课助手实现网课自动刷课/","2024/05/05/同款博客搭建教程/","2024/05/09/虚拟机安装W7-W10优化精简版/","2024/05/10/python学习笔记一基础语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
